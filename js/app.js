8a2034a63f3232e9fc31c7a66996967cde0aa63b
tshifttwo <tatianac88@gmail.com>
2016-05-18 20:05:20 -0400

    pages linked together

--- js/app.js
+++ js/app.js
@@ -1,9 +1,57 @@
 $(document).ready(function(){
-	// $("#welcome").fadeIn(2000);
+	
 
-$("#startbutton").click(function(){
-	$("#welcome").fadeout(2000);
-	$("#firstquestion").fadein(2000);
-});
+	$('button').click(function(){
+		$(".intropage").fadeOut(2000);
+		$(".question1").fadeIn(2000);
+
+	});
+
+	$(".question1").click(function(){
+		$(".question1").fadeOut(2000);
+		$(".question2").fadeIn(2000);
+	});
+
+	$(".question2").click(function(){
+		$(".question2").fadeOut(2000);
+		$(".question3").fadeIn(2000);
+	});
+
+	$(".question3").click(function(){
+		$(".question3").fadeOut(2000);
+		$(".question4").fadeIn(2000);
+	});
+
+	$(".question4").click(function(){
+		$(".question4").fadeOut(2000);
+		$(".question5").fadeIn(2000);
+	});
+
+	$(".question5").click(function(){
+		$(".question5").fadeOut(2000);
+		$(".Williamsburgresult").fadeIn(2000);
+	});
+
+	$(".Williamsburgresult").click(function(){
+		$(".Williamsburgresult").fadeOut(2000);
+		$(".CHeightsresult").fadeIn(2000);
+	});
+
+	$(".CHeightsresult").click(function(){
+		$(".CHeightsresult").fadeOut(2000);
+		$(".dumboresult").fadeIn(2000);
+	});
+
+	
+	$(".dumboresult").click(function(){
+		$(".dumboresult").fadeOut(2000);
+		$(".parkslopeesult").fadeIn(2000);
+	});
+
+	$(".parkslopeesult").click(function(){
+		$(".parkslopeesult").fadeOut(2000);
+		$(".intropage").fadeIn(2000);
+	});
+
 
 });
