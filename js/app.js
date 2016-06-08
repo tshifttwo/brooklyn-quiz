9eefc36e0e526c0d485eb9f256ac7ee9fcf96f24
tshifttwo <tatianac88@gmail.com>
2016-06-08 08:53:59 -0400

    some logic

--- js/app.js
+++ js/app.js
@@ -53,5 +53,163 @@ $(document).ready(function(){
 		$(".intropage").fadeIn(2000);
 	});
 
+var neighborhood = {
+		"williamsburg": 0, //properties
+		"crownheights":0,
+		"brooklynheights":0,
+		"dumbo":0,
+		"bushwick":0,
+		"parkslope":0,
+	};
+
+var questions = [
+	{
+		// "Q1: "Which subway line do you need access to the most?",
+		"Q1":[
+			{
+				"crownheights":2,
+				"dumbo":2,
+			},
+			{ //no 2
+				"brooklynheights":2,
+				"dumbo":2,
+			},
+			{//3
+				"williamsburg":2,
+				"bushwick":2,
+			},
+			{//4
+				"brooklynheights":2,
+				"parkslope":2,
+			},
+			{//5
+				"williamsburg":2,
+				"bushwick":2,
+			},
+			{//6
+				"williamsburg":2,
+				"crownheights":2,
+			},
+		],
+
+		// Q2: "What kind of shopping do you want nearby?",
+		"Q2":[
+			{
+				"williamsburg":1,
+				"bushwick":1,
+				"brooklynheights":1,
+				"dumbo":1,
+			},
+			{ //no 2
+				"williamsburg":1,
+				"bushwick":2,
+				"crownheights":1,
+			},
+			{//3
+				"crownheights":1,
+				"parkslope":1,
+				"brooklynheights":1,
+				"dumbo":1,
+			},
+			{//4
+				"brooklynheights":2,
+				"dumbo":1,
+				"parkslope":1
+			},
+			{//5
+				"williamsburg":1,
+				"bushwick":1,
+				"parkslope":1,
+				"dumbo":1,
+			},
+			{//6
+				"williamsburg":1,
+				"crownheights":2,
+				"parkslope":1,
+			},
+		],
+
+		// Q3: "Which celebrity neighbor would you like to have?",
+		"Q3":[
+			{
+				"brooklynheights":2,
+			},
+			{ //no 2
+				"bushwick":2,
+			},
+			{//3
+				"parkslope":2,
+			},
+			{//4
+				"dumbo":2,
+			},
+			{//5
+				"crownheights":2,
+			},
+			{//6
+				"williamsburg":2,
+			},
+		],
+
+		// Q4: "Which bar theme is your favorite?",
+		"Q4":[
+			{
+				"williamsburg":2,
+			},
+			{ //no 2
+				"dumbo":2,
+			},
+			{//3
+				"brooklynheights":2,
+			},
+			{//4
+				"parkslope":2,
+			},
+			{//5
+				"crownheights":2,
+			},
+			{//6
+				"bushwick":2,
+			},
+		],
+
+		// Q5: "Which of these is important to you?",
+		"Q5":[
+			{
+				"williamsburg":1,
+				"dumbo":1,
+				"brooklynheights":1,
+				"parkslope":1,
+			},
+			{ //no 2
+				"williamsburg":2,
+				"bushwick":1,
+				"crownheights":1
+			},
+			{//3
+				"williamsburg":1,
+				"brooklynheights":1,
+				"dumbo":2,
+			},
+			{//4
+				"bushwick":1,
+				"crownheights":2,
+				"parkslope":1,
+			},
+			{//5
+				"dumbo":1,
+				"brooklynheights":2,
+				"parkslope":1,
+			},
+			{//6
+				"bushwick":2,
+				"crownheights":1,
+				"parkslope":1, 
+			},
+		],
+	}
+]
+
+	
 
 });
--- notes.js
+++ notes.js
@@ -1,8 +1,39 @@
+var
+
+on ".nextquestion" click go to next page 
+on answer 1 add 2 points to brooklynheights
+			add 2 points to dumbo
+
+select answers as chosen
+at the end, add them up
+
+classes for each questions
+compile results
+
+for each question, find me the answer with the class chosen, and then get the index of that answer, 
+
+assign a data answer to each question
+
+each neighborhood key in the answerneighborhood[key] += answer[key];
+
+
+var neighborhood = {
+		"williamsburg": 0, //properties
+		"crownheights":0,
+		"brooklynheights":0,
+		"dumbo":0,
+		"bushwick":0,
+		"parkslope":0,
+	}	
+
+
+
+
 var user = {
 	points:{
 		"williamsburg": 0, //properties
 		"crownheights":0,
-		"brooklynheight":0,
+		"brooklynheights":0,
 		"dumbo":0,
 		"bushwick":0,
 		"parkslope":0,
@@ -11,29 +42,40 @@ var user = {
 
 /*arrays only have values*/
 
+// do i really need "answers" array?
+
+
 var questions = [
 	{
-		"questiontext": "Which subway line do you need access to the most?",
+		// "questiontext": "Which subway line do you need access to the most?",
 		"answers":[
 			{
-				"value":'<img src="Exports/123.png" alt="1-2-3" style="width:auto;height:50px;">',
+				// "value":'<img src="Exports/123.png" alt="1-2-3" style="width:auto;height:50px;">',
 				"points":{
-					"crownheights":2,
+					"brooklynheights":2,
 					"dumbo":2,
 				}
 
-			}, {}, {}, {}, {}, {},
+			}, 
+			{
+				"value":'<img src="Exports/ACE.png" alt="A-C-E" style="width:auto;height:50px;">',
+				"points":{
+					"crownheights":2,
+					"dumbo":2,
+				
+			}, {}, {}, {}, {},
 
 		]
-
 		
-	}, {}, {}, {}, {},
+	}, {
+
+	}, {}, {}, {},
 ]
 
 var questionindex = 0;
 var currentquestion = questions[questionindex];
 
-$(kjsdf).on('click', '.answer', function() {
+$(".nextquestion").on('click', '.answer', function() {
 	var answerindex = $(this).data('answer-index');
 	var currentanswer = currentquestion.answers[answerindex];
 
